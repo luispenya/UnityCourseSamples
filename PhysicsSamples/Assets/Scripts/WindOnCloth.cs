7733fc0b9c6eecc73bfd5af12d2c7bce70f0309f
Luis Peña <luis.penya@lurtis.com>
2016-04-17 22:38:14 +0200

    Physics Sample WindCloth

---
+++ PhysicsSamples/Assets/Scripts/WindOnCloth.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WindOnCloth : MonoBehaviour {
+
+    public Cloth pieceOfCloth;
+    public WindZone wZ;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log("TR"+wZ.transform.forward);
+
+            Vector3 str = -wZ.transform.forward * wZ.windMain*20;
+            pieceOfCloth.randomAcceleration = str;
+        
+	}
+}
