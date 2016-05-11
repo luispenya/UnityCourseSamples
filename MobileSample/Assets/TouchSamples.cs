87bf3bbc6bda312fe872366ed20bb67d39536b9f
Luis Peña <luis.penya@lurtis.com>
2016-05-11 20:29:54 +0200

    Acelerometro y Multitouch con CrossPlatform Controller.

--- MobileSample/Assets/TouchSamples.cs
+++ MobileSample/Assets/TouchSamples.cs
@@ -1,6 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+using UnityStandardAssets.CrossPlatformInput;
+
 public class TouchSamples : MonoBehaviour {
 
 
@@ -10,19 +12,42 @@ public class TouchSamples : MonoBehaviour {
 
     void Update () {
 
-        
-        if (Input.touchCount>0)
+        if (CrossPlatformInputManager.GetAxis("Horizontal") > 0f)
         {
-            Touch toque = Input.GetTouch(0);
-
-
-            if (toque.phase == TouchPhase.Stationary)
-            {
-                cube.transform.Translate(cube.transform.forward * Time.deltaTime);
-            }
+            cube.transform.Translate(new Vector3(0f, CrossPlatformInputManager.GetAxis("Horizontal"), 0f));
         }
+        else
+        {
 
-        accel = new Vector3(Input.acceleration.x, Input.acceleration.y, Input.acceleration.z);
+            if (Input.touchCount > 0)
+            {
+                Touch toque = Input.GetTouch(0);
+
+
+                if (toque.phase == TouchPhase.Stationary)
+                {
+                    //cube.transform.Translate(cube.transform.forward * Time.deltaTime);
+
+                }
+                if (toque.phase == TouchPhase.Moved)
+                {
+                    cube.transform.Translate(
+                        Vector3.Lerp(cube.transform.position,
+                        Camera.main.ScreenToWorldPoint(toque.position), 0.1f));
+
+                }
+
+                Touch[] lista = Input.touches;
+                for (int i = 0; i < Input.touchCount; i++)
+                {
+                    Debug.Log("Pinchado" + lista[i].fingerId);
+                }
+            }
+
+            accel = new Vector3(Input.acceleration.x, Input.acceleration.y, Input.acceleration.z);
+            cube.transform.Rotate(accel);
+
+        }
         
 	}
 
